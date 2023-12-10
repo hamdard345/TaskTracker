37f767c47960e72b0d34ff036c735571dcb1d7d5
hamdard345 <58349754+hamdard345@users.noreply.github.com>
2023-12-10 20:08:11 +0000

    fixed not an array error on tasklist

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,14 +1,14 @@
 import './taskList.css'
 
 function TaskList({tasks}) {
-  const displayTasks = tasks.map((task,index) =>{
+  const displayTasks = Array.isArray(tasks) ? tasks.map((task,index) =>{
     return (
       <div key={index}>
         <p>Title : {task.taskTitle}</p>
         <p>Task : {task.inputText}</p>
       </div>
     )
-  })
+  }) : null;
   return (
     <div className='container'>
       {displayTasks}
